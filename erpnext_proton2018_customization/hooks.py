a000db7781315560e3f2e25177d0a0ae3a572d13
Armando Hdez <mexarm@me.com>
2018-04-18 13:37:46 -0500

    added Grafica de Impresion, Spool Entry DocType

--- erpnext_proton2018_customization/hooks.py
+++ erpnext_proton2018_customization/hooks.py
@@ -119,4 +119,4 @@ app_license = "MIT"
 # override_whitelisted_methods = {
 # 	"frappe.desk.doctype.event.event.get_events": "erpnext_proton2018_customization.event.get_events"
 # }
-fixtures = ["Custom Field", "Custom Script", "Property Setter", "Print Format"]
+fixtures = ["Custom Field", "Custom Script", "Property Setter", "Print Format", {"dt" : "DocType", "filters": [["name", "in", ("Grafica de Impresion","Spool Entry")]]}]
