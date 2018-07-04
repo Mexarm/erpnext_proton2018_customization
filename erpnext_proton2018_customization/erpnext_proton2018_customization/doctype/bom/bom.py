99caab231fba6a2fd963e137418bf763f6226136
Armando Hdez <mexarm@me.com>
2018-07-04 02:39:17 -0500

    Sales Order, consider Working Days between start and delivery date

--- erpnext_proton2018_customization/erpnext_proton2018_customization/doctype/bom/bom.py
+++ erpnext_proton2018_customization/erpnext_proton2018_customization/doctype/bom/bom.py
@@ -30,10 +30,11 @@ def get_raw_materials_cost_for_item(item_code):
 def get_raw_materials_cost_and_sla_for_item(item_code):
         bom_no = get_default_bom_item(item_code)
         bom = frappe.get_doc("BOM" , bom_no)
-        if bom: return { 'raw_material_cost': bom.raw_material_cost, 'nivel_de_servicio': bom.nivel_de_servicio }
+        hora_limite_de_entrega_de_archivos = frappe.db.get_single_value("Proton Setup", "hora_limite_de_entrega_de_archivos")
+        if bom: return { 'raw_material_cost': bom.raw_material_cost, 'nivel_de_servicio': bom.nivel_de_servicio, 'hora_limite_de_entrega_de_archivos': hora_limite_de_entrega_de_archivos }
         item = frappe.get_doc("Item", item_code)
-        if item: return { 'raw_material_cost': item.valuation_rate, 'nivel_de_servicio': 0 }
-        return { 'raw_material_cost': 0.0, 'nivel_de_servicio': 0 }
+        if item: return { 'raw_material_cost': item.valuation_rate, 'nivel_de_servicio': 0, 'hora_limite_de_entrega_de_archivos': hora_limite_de_entrega_de_archivos }
+        return { 'raw_material_cost': 0.0, 'nivel_de_servicio': 0, 'hora_limite_de_entrega_de_archivos': hora_limite_de_entrega_de_archivos  }
 
 @frappe.whitelist()
 def default_bom_item_has_adicionales_variables(item_code):
